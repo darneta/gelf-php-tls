440127ea01983dfcae1ef739cc34bb9c1fd09ded
Darius Krištapavičius <darius@darneta.lt>
2018-01-09 15:04:58 +0200

    Initial commit

---
+++ src/TlsTransport.php
@@ -0,0 +1,24 @@
+<?php
+/*
+ * This file is part of the php-gelf package.
+ *
+ * (c) Darneta <dev@darneta.lt>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Darneta\GelfPhpTls;
+
+use Gelf\Transport\TcpTransport;
+
+class TlsTransport extends TcpTransport
+{
+    public function __construct($host = self::DEFAULT_HOST, $port = self::DEFAULT_PORT, SslOptions $sslOptions = null)
+    {
+        parent::__construct($host, $port);
+
+        $context = $sslOptions ? $sslOptions->toStreamContext() : array();
+        $this->socketClient = new StreamSocketClient('tls', $host, $port, $context);
+    }
+}
